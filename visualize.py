e50880ae2a55d5eb3643e65caaa031d8aff252af
1jsingh <1.jaskiratsingh@gmail.com>
2018-12-07 17:25:42 +0530

    update

--- visualize.py
+++ visualize.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
-%matplotlib inline
+#%matplotlib inline
 
 from ddpg_agent import Agent
 
@@ -17,32 +17,32 @@ env = gym.make('BipedalWalker-v2')
 env.seed(10)
 agent = Agent(state_size=env.observation_space.shape[0], action_size=env.action_space.shape[0], random_seed=10)
 
-def ddpg(n_episodes=2000, max_t=700):
-    scores_deque = deque(maxlen=100)
-    scores = []
-    max_score = -np.Inf
-    for i_episode in range(1, n_episodes+1):
-        state = env.reset()
-        agent.reset()
-        score = 0
-        for t in range(max_t):
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            agent.step(state, action, reward, next_state, done)
-            state = next_state
-            score += reward
-            if done:
-                break 
-        scores_deque.append(score)
-        scores.append(score)
-        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
-        if i_episode % 100 == 0:
-            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
-    return scores
+# def ddpg(n_episodes=2000, max_t=700):
+#     scores_deque = deque(maxlen=100)
+#     scores = []
+#     max_score = -np.Inf
+#     for i_episode in range(1, n_episodes+1):
+#         state = env.reset()
+#         agent.reset()
+#         score = 0
+#         for t in range(max_t):
+#             action = agent.act(state)
+#             next_state, reward, done, _ = env.step(action)
+#             agent.step(state, action, reward, next_state, done)
+#             state = next_state
+#             score += reward
+#             if done:
+#                 break 
+#         scores_deque.append(score)
+#         scores.append(score)
+#         print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
+#         if i_episode % 100 == 0:
+#             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+#             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+#             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
+#     return scores
 
-scores = ddpg()
+# scores = ddpg()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
