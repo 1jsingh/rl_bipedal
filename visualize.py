a630912436677f02eac50ce188a402a204e7297d
1jsingh <1.jaskiratsingh@gmail.com>
2018-12-07 17:38:34 +0530

    update

--- visualize.py
+++ visualize.py
@@ -17,36 +17,35 @@ env = gym.make('BipedalWalker-v2')
 env.seed(10)
 agent = Agent(state_size=env.observation_space.shape[0], action_size=env.action_space.shape[0], random_seed=10)
 
-# def ddpg(n_episodes=2000, max_t=700):
-#     scores_deque = deque(maxlen=100)
-#     scores = []
-#     max_score = -np.Inf
-#     for i_episode in range(1, n_episodes+1):
-#         state = env.reset()
-#         agent.reset()
-#         score = 0
-#         for t in range(max_t):
-#             action = agent.act(state)
-#             next_state, reward, done, _ = env.step(action)
-#             agent.step(state, action, reward, next_state, done)
-#             state = next_state
-#             score += reward
-#             if done:
-#                 break 
-#         scores_deque.append(score)
-#         scores.append(score)
-#         print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
-#         if i_episode % 100 == 0:
-#             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-#             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-#             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
-#     return scores
+agent.actor_local.load_state_dict(torch.load('checkpoint_actor.pth'))
+agent.critic_local.load_state_dict(torch.load('checkpoint_critic.pth'))
 
-# scores = ddpg()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(np.arange(1, len(scores)+1), scores)
-plt.ylabel('Score')
-plt.xlabel('Episode #')
-plt.show()
+# function to animate a list of frames
+def animate_frames(frames):
+    plt.figure(dpi = 72)
+    plt.axis('off')
+
+    # color option for plotting
+    # use Greys for greyscale
+    cmap = None if len(frames[0].shape)==3 else 'Greys'
+    patch = plt.imshow(frames[0], cmap=cmap)  
+
+    fanim = animation.FuncAnimation(plt.gcf(), \
+        lambda x: patch.set_data(frames[x]), frames = len(frames), interval=30)
+    
+    display(display_animation(fanim, default_mode='once'))
+    
+frames = []
+state = env.reset()
+agent.reset()
+for t in range(1000):
+    action = agent.act(state)
+    frames.append(env.render(mode='rgb_array')) 
+    next_state, reward, done, _ = env.step(action)
+    state=next_state
+    if done:
+        break
+
+env.close()
+animate_frames(frames)
