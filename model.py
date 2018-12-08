b502d6938c34119089c0ac0f3779526c0e4d5e03
1jsingh <1.jaskiratsingh@gmail.com>
2018-12-08 11:31:16 +0000

    update

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ class Actor(nn.Module):
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc_units)
         self.fc2 = nn.Linear(fc_units, action_size)
-        self.reset_parameters()
+        #self.reset_parameters()
 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
@@ -41,7 +41,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc2_units=256, fc3_units=128):
+    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=64, fc3_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -55,9 +55,9 @@ class Critic(nn.Module):
         self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, 1)
-        self.reset_parameters()
+        #self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.fc4 = nn.Linear(fc2_units, 1)
+        #self.reset_parameters()
 
     def reset_parameters(self):
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
@@ -70,5 +70,5 @@ class Critic(nn.Module):
         xs = F.leaky_relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        #x = F.leaky_relu(self.fc3(x))
         return self.fc4(x)
